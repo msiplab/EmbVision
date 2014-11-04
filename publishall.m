8faba25aaae0370f90167adaf88a855ca518455f
shodimaggio <shogo@eng.niigata-u.ac.jp>
2014-11-05 01:27:31 +0900

    Part 1 was completed.

--- publishall.m
+++ publishall.m
@@ -1,8 +1,39 @@
 % PUBLISHALL
-publish('index');
-publish('part1');
-publish('part2');
-publish('part3');
-publish('part4');
-publish('part5');
-publish('part6');
+type = 'html';
+
+%% Index
+publish('index',type);
+
+%% Part 1
+
+I = im2double((imread('peppers.png')));
+J = sqrt(I);
+imwrite(J,'html/brightpeppers.png')
+fplot(@sqrt,[0 1])
+xlabel('x')
+ylabel('y')
+axis square
+print -dpng -r72 'html/fplotsqrt.png'
+
+HSV = rgb2hsv(I);
+HSV(:,:,2) = 2*HSV(:,:,2);
+J = hsv2rgb(HSV);
+imwrite(J,'html/highsatpeppers.png')
+
+publish('part1',type);
+
+%% Part 2
+publish('part2',type);
+
+%% Part 3
+publish('part3',type);
+
+%% Part 4
+publish('part4',type);
+
+%% Part 5
+publish('part5',type);
+
+%% Part 6
+publish('part6',type);
+
