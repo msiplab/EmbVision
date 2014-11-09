e3105224998d0e34b6bb196392d987fb431fa157
shodimaggio <shogo@eng.niigata-u.ac.jp>
2014-11-09 13:46:33 +0900

    Part 2 完了

--- publishall.m
+++ publishall.m
@@ -5,7 +5,9 @@ type = 'html';
 publish('index',type);
 
 %% Part 1
+close all
 
+% ‰Û‘è 1-1
 I = im2double((imread('peppers.png')));
 J = sqrt(I);
 imwrite(J,'html/brightpeppers.png')
@@ -15,14 +17,42 @@ ylabel('y')
 axis square
 print -dpng -r72 'html/fplotsqrt.png'
 
+% ‰Û‘è 1-2
 HSV = rgb2hsv(I);
 HSV(:,:,2) = 2*HSV(:,:,2);
 J = hsv2rgb(HSV);
 imwrite(J,'html/highsatpeppers.png')
 
+% Publish
 publish('part1',type);
 
 %% Part 2
+close all
+
+% ‰Û‘è 2-1
+X = imread('cameraman.tif');
+X = im2double(X);
+
+Hh = [ 1 0 -1; 1 0 -1; 1 0 -1 ];
+Yh = conv2(Hh,X);
+Yh = Yh(2:end-1,2:end-1);
+imwrite(Yh+0.5,'html/cameramangradx.png')
+
+freqz2(Hh)
+print -dpng -r72 'html/freqz2gradx.png'
+
+% ‰Û‘è 2-2
+Hv = [ 1 1 1; 0 0 0; -1 -1 -1 ];
+Yv = conv2(Hv,X);
+Yv = Yv(2:end-1,2:end-1);
+
+dXmag = sqrt(Yv.^2+Yh.^2);
+dXang = atan2(Yv,Yh);
+
+imwrite(dXmag,'html/cameramangradmag.png')
+imwrite(dXang/(2*pi)+0.5,'html/cameramangradang.png')
+
+% Publish
 publish('part2',type);
 
 %% Part 3
