1ab90c4b283757156bbd10ef8a1c0c022f9618ec
shodimaggio <shogo@eng.niigata-u.ac.jp>
2022-06-06 11:28:49 +0900

    フォルダを再編成

--- workcq/part4/PolyfitSystem.m
+++ workcq/part4/PolyfitSystem.m
@@ -1,8 +1,16 @@
 classdef PolyfitSystem < matlab.System
-% POLYFITSYSTEM 二値化画像から多項式近似をおこなう
-    properties (Nontunable)
-        % Nontunableでプロパティをプログラムの途中で変更されるのを防ぐ
-        Deg = 2;    % 多項式の次数
+    % POLYFITSYSTEM
+    properties(Nontunable)
+        Degree = 3;
+    end
+
+    properties(DiscreteState)
+
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+
     end
     methods
        % コンストラクタ
@@ -12,25 +20,18 @@ classdef PolyfitSystem < matlab.System
     end
     methods(Access = protected)
         function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
         end
-        function P = stepImpl(obj,BW)
-            [y,x] = find(BW);   % 画像を座標に変換
-            P = polyfit(x,y,obj.Deg);   % 多項式近似
+
+        function p = stepImpl(obj,BW)
+            % Implement algorithm. Calculate y as a function of input u and
+            % discrete states.
+            [y,x] = find(BW); % 画像を座標に変換
+            p = polyfit(x,y,obj.Degree); % 多項式近似
         end
+
         function resetImpl(obj)
             % Initialize / reset discrete-state properties
         end
-        % 入力ポート名
-        function inputName = getInputNamesImpl(obj)
-            inputName = 'BW';
-        end
-        % 出力ポート名        
-        function outputName = getOutputNamesImpl(obj)
-            outputName = 'P';
-        end
-        % アイコン
-        function icon = getIconImpl(obj)
-           icon = 'BW to P';
-        end        
     end
 end
